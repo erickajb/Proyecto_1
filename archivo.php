b97b6a684106b996b0d12c84463785340ac42227
erickajb88 <erickajb88>
2014-02-16 22:10:17 -0600

    Formulario listo y guarda en el csv bien

--- archivo.php
+++ archivo.php
@@ -1,4 +1,12 @@
 
 <?php
 
+//Variables que obtienen la información del formulario
+$nombre = $_POST["name"];
+$apellido= $_POST["lastname"];
+$correo = $_POST["email"];
+$telefono = $_POST["phone"];
+$cedula =$_POST["id"];
+
+
 ?>
