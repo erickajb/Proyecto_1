c8346e270aae33d8a24781af388d22fd3cb0d7f0
erickajb88 <erickajb88>
2014-02-16 22:13:25 -0600

    Revert "Formulario listo y guarda en el csv bien"
    This reverts commit b97b6a684106b996b0d12c84463785340ac42227.

--- archivo.php
+++ archivo.php
@@ -1,12 +1,4 @@
 
 <?php
 
-//Variables que obtienen la información del formulario
-$nombre = $_POST["name"];
-$apellido= $_POST["lastname"];
-$correo = $_POST["email"];
-$telefono = $_POST["phone"];
-$cedula =$_POST["id"];
-
-
 ?>
