4e5e04890657a9b99b2050c8227a8ec39f5a171b
erickajb88 <erickajb88>
2014-02-18 22:48:19 -0600

    Guarda la fecha correctamente
    La ultima actualización guardada un error en la fecha

--- post.php
+++ post.php
@@ -4,6 +4,8 @@ $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $id = $_POST['id'];
+date_default_timezone_set("America/Costa_Rica");
+$dateSystem=date("dmY");
 
 
 //validate
@@ -17,7 +19,7 @@ $aClass = 'errorClass';
 //this is where the creating of the csv takes place
 $cvsData = $name . ";" . $lastname . ";" . $email . ";". $phone . ";" . $id . ";" ."\n";
 
-$fp = fopen("archivo.csv","a"); // $fp is now the file pointer to file $filename
+$fp = fopen($dateSystem.".csv","a"); // $fp is now the file pointer to file $filename
 
 if($fp){
 fwrite($fp,$cvsData); // Write information to the file
