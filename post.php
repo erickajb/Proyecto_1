8afa71cf230a17b9659c29063912a8c42c99b373
erickajb88 <erickajb88>
2014-02-23 11:06:44 -0600

    Carpeta  de style separada

--- post.php
+++ post.php
@@ -8,21 +8,15 @@ date_default_timezone_set("America/Costa_Rica");
 $dateSystem=date("dmY");
 
 
-//validate
-
-if(empty($name) || empty($lastname) || empty($email) || empty($phone) || empty($id)){
-
-$message = 'Fill in areas in red!';
-$aClass = 'errorClass';
-}
-
 //this is where the creating of the csv takes place
 $cvsData = $name . ";" . $lastname . ";" . $email . ";". $phone . ";" . $id . ";" ."\n";
 
 $fp = fopen($dateSystem.".csv","a"); // $fp is now the file pointer to file $filename
 
 if($fp){
-fwrite($fp,$cvsData); // Write information to the file
-fclose($fp); // Close the file
+	fwrite($fp,$cvsData); // Write information to the file
+	fclose($fp); // Close the file
 }
+
+
 ?>
