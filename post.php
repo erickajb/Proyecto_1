3cdfffef59582098012e3d526b7d061e102876fc
erickajb88 <erickajb88>
2014-02-25 23:19:23 -0600

    Parte del correo funcionaldo

--- post.php
+++ post.php
@@ -1,21 +1,21 @@
 <?php
-
+//Variables que obtiene los datos que se introducen en el formulario
 $name = $_POST['name'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $id = $_POST['id'];
-date_default_timezone_set("America/Costa_Rica");
-$dateSystem=date("dmY");
+date_default_timezone_set("America/Costa_Rica");	// Configuramos la zona horaria para que nos de la fecha exacta
+$dateSystem=date("dmY");	//Variable que guarda la fecha del sistema
 
 
-//this is where the creating of the csv takes place
-$cvsData = $name . ";" . $lastname . ";" . $email . ";". $phone . ";" . $id . ";" ."\n";
 
-$fp = fopen($dateSystem.".csv","a"); // $fp is now the file pointer to file $filename
+$cvsData = $name . ";" . $lastname . ";" . $email . ";". $phone . ";" . $id . ";" ."\n";	//aquí es donde creamos nuestro archivo csv
+
+$fp = fopen($dateSystem.".csv","a");
 
 if($fp){
-	fwrite($fp,$cvsData); // Write information to the file
-	fclose($fp); // Close the file
+	fwrite($fp,$cvsData);	 // Se escribe la información en el archivo csv
+	fclose($fp); 	// Cerramos nuestro archivo
 }
 ?>
--- script.php
+++ script.php
@@ -1,17 +1,17 @@
 <?php
     
-function Conectarse() //Función para conectarse a la BD
+function Conectarse()    //Función para conectarse a la BD
 {
-    global $argv;//Variable para obtener el nombre digitado por parametro
+    global $argv;   //Variable para obtener el nombre digitado por parametro
     $_SERVER["argv"];
     $parameterJson=$argv[1];
 
-    $json = file_get_contents($parameterJson . ".json");//Guardamos los datos que obtuvimos de esta funcion en la variable  llamada json.
-    $data = json_decode($json, true);//esta funcion nos dara los datos de json en un formato que php pueda leer adecuadamente, y la guardaremos en otra variable llamada data.
-    $IP=$data["ip"];//Variable que guarda lo ip del servidor
-    $USER=$data["user"];//Variable que guarda el user
-    $PASSWORD=$data["password"];//Variable que guarda el password
-    $DB=$data["db"];//Variable que guarda el nombre de nuestra base de datos
+    $json = file_get_contents($parameterJson . ".json");    //Guardamos los datos que obtuvimos de esta funcion en la variable  llamada json.
+    $data = json_decode($json, true);   //esta funcion nos dara los datos de json en un formato que php pueda leer adecuadamente, y la guardaremos en otra variable llamada data.
+    $IP=$data["ip"];    //Variable que guarda lo ip del servidor
+    $USER=$data["user"];    //Variable que guarda el user
+    $PASSWORD=$data["password"];    //Variable que guarda el password
+    $DB=$data["db"];    //Variable que guarda el nombre de nuestra base de datos
 
    if (!($link = mysqli_connect($IP, $USER, $PASSWORD, $DB)))
     {
@@ -19,30 +19,55 @@ function Conectarse() //Función para conectarse a la BD
         exit();
     }
     return $link;
+
 }
+
 date_default_timezone_set("America/Costa_Rica");
 $dateSystem=date("dmY");
-$row    = 0;
-$handle = fopen($dateSystem . ".csv", "r"); // nombre del archivo csv que contiene los datos
-while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) { //Lee toda una linea completa y ingresa los datos en el array 'data'
-    $num = count($data); //Cuenta cuantos campos contiene la linea (el array 'data')
+$row = 0;
+$handle = fopen($dateSystem . ".csv", "r");  //nombre del archivo csv que contiene los datos
+while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {   //Lee toda una linea completa y ingresa los datos en el array 'data'
+    $num = count($data);    //Cuenta cuantos campos contiene la linea (el array 'data')
     $row++;
-    $cadena = "INSERT INTO estudiantes_ingresados (Name,Last_Name,Email,Phone,Id) VALUES ("; //campos de la tabla
+    $cadena = "INSERT INTO estudiantes_ingresados (Name,Last_Name,Email,Phone,Id) VALUES (";     //campos de la tabla
     
-    for ($c = 0; $c < $num; $c++) { //Aquí va colocando los campos en la cadena, si aun no es el último campo, le agrega la coma (,) para separar los datos
+    for ($c = 0; $c < $num; $c++) {  //Aquí va colocando los campos en la cadena, si aun no es el último campo, le agrega la coma (,) para separar los datos
         if ($c == ($num - 1))
             $cadena = $cadena . "'" . $data[$c] . "'";
         else
             $cadena = $cadena . "'" . $data[$c] . "',";
     }
     
-    $cadena = $cadena . ");"; //Termina de armar la cadena para poder ser ejecutada
-    echo $cadena; //Muestra la cadena para ejecutarse
-    
+    $cadena = $cadena . ");\n";   //Termina de armar la cadena para poder ser ejecutada
+    echo $cadena;   //Muestra la cadena para ejecutarse
     $enlace = Conectarse();
-    $result = mysqli_query($enlace, $cadena); //Aquí está la clave, se ejecuta con MySQL la cadena del insert formada
+    $result = mysqli_query($enlace, $cadena);   //Aquí está la clave, se ejecuta con MySQL la cadena del insert formada
     mysqli_close($enlace);
 }
 fclose($handle);
 
+include("PHPMailer/class.phpmailer.php"); 
+include("PHPMailer/class.smtp.php"); 
+$mail = new PHPMailer(); 
+$mail->IsSMTP(); 
+$mail->SMTPAuth = true; 
+$mail->SMTPSecure = "ssl"; 
+$mail->Host = "smtp.gmail.com"; 
+$mail->Port = 465; 
+$mail->Username = "erickajb88@gmail.com"; 
+$mail->Password = "2-691-563";
+$mail->From = "erickajb88@gmail.com"; 
+$mail->FromName = "Ericka Jimenez Barquero"; 
+$mail->Subject = "Confirmation"; 
+$mail->MsgHTML("<b>Records inserted in the database are: </b>" . $row); 
+$mail->AddAddress("erickajb88@gmail.com", "Ericka Jimenez Barquero"); 
+$mail->IsHTML(true);
+
+if(!$mail->Send()) {  
+    echo "Error:". $Mail-> ErrorInfo;  
+ } 
+ else 
+ {  
+    echo "\n Message sent successfully";  
+ }
 ?>
